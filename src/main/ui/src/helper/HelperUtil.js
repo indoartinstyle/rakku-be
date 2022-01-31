82e9ad364bbea82da782a40effa4559280082f1a
Sanjay Das <sanjaydas.32100@gmail.com>
2022-01-31 23:59:53 +0530

    T (#3)
    * UI Added (#1)

--- src/main/ui/src/helper/HelperUtil.js
+++ src/main/ui/src/helper/HelperUtil.js
@@ -1,5 +1,5 @@
 export default function getFullUrl(uriPath) {
-  let host = window.location.port == 3000 ? 'http://127.0.0.1:8080' : '';
+  let host = window.location.port === '3000' ? 'http://127.0.0.1:8080' : '';
   let finalUrl = host + uriPath
   console.debug('Final URL', finalUrl);
   return finalUrl;
