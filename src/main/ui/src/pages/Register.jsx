ba7beeb65a870e0ffffadc9a86f21b45347dd994
Sanjay Das - s0d062y <sanjay.das@walmart.com>
2022-02-03 00:02:18 +0530

    Fixed Build issue

--- src/main/ui/src/pages/Register.jsx
+++ src/main/ui/src/pages/Register.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Footer from '../components/Footer.jsx';
 import Navbar from '../components/Navbar.jsx';
-import Jumbotron from '../components/Jumbotron.jsx';
 
 class Register extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar/>
         <div className="container">
           <h2>Form Will appear here</h2>
         </div>
-        <Footer />
+        <Footer/>
       </div>
     );
   }
